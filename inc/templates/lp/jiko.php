55d08d4496cc7492103d5079a2060c963e93d67c
kazuki-nisshin <nisshin@dtdsh.com>
2017-06-22 11:24:25 +0900

    LP完成

--- inc/templates/lp/jiko.php
+++ inc/templates/lp/jiko.php
@@ -48,7 +48,7 @@
 			</div>
 			<div class="jiko-up_wrap">
 				<h3 class="jiko-up_title">③裁判基準</h3>
-				<p class="jiko-up_desc">過去の交通事故に関する裁判の判例などを踏まえて、損害の内容ごとに基準が示されています。弁護士が介入すると示談交渉からこの基準で計算します。<br>弁護士報酬を差し引いても、お手元に残る賠償金が増える場合が多いですので、<span class="bg-line">「弁護士費用のお支払い」を考慮しても、弁護士に任せるほうがメリットが多い</span>です。</p>
+				<p class="jiko-up_desc">過去の交通事故に関する裁判の判例などを踏まえて、損害の内容ごとに基準が示されています。弁護士が介入すると示談交渉からこの基準で計算します。<br>弁護士報酬を差し引いても、お手元に残る賠償金が増える場合が多いですので、<span class="bg-line">「弁護士費用のお支払い」を考慮しても、弁護士に任せることをお勧め</span>します。</p>
 			</div>
 		</div>
 		<div class="column small-12 large-6 text-center"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/lp/jiko/graph.png'; ?>" alt="弁護士介入による賠償金（慰謝料・給与損害・遺失利益など）が増加する一般的な例。"></div>
@@ -63,7 +63,7 @@
 				<h4>後遺障害の事故</h4>
 				<div class="img-wrapper"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/traffic-acc-cases_02.png'; ?>"
 					    alt="1010万円の増額"></div>
-				<p>　依頼者は、タクシー乗車中にタクシーが交通事故に遭い、局部に神経症状を残すとして、<span class="bg-line">14級の後遺障害認定</span>を受けた。加害者の任意保険会社は、治療費等すでに約310万を支払済みであったところ、症状が固定したので、賠償金として残金200万円を支払って示談を成立させたい旨を提案してきた（
+				<p>　依頼者は、タクシー乗車中に交通事故に遭い、局部に神経症状を残すものとして、<span class="bg-line">14級の後遺障害認定</span>を受けた。加害者の任意保険会社は、治療費等ですでに約310万を支払済みであったところ、症状が固定したので、賠償金として残金200万円を支払って示談を成立させたい旨を提案してきた（
 					<span class="bg-line">保険会社基準では損害額合計は約500万円ということ</span>）。<br>　当事務所において、<span class="bg-line">裁判基準</span>に基づいて計算し、
 					<span class="bg-line">担当弁護士が保険会社と交渉。</span>結果、保険会社提示額の残金200万円から、
 					<span class="bg-line">300万円</span>増額し、残金500万円で示談が成立した。</p>
@@ -72,23 +72,22 @@
 				<h4>後遺障害の事故</h4>
 				<div class="img-wrapper"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/traffic-acc-cases_04.png'; ?>"
 					    alt="300万円の増額"></div>
-				<p>　依頼者が自転車で横断中、トラックが側面から衝突、依頼者には<span class="bg-line">右足関節の機能障害他の後遺障害</span>が残り、<span class="bg-line">併合11級と認定</span>された。保険会社は未払分として
-					<span class="bg-line">保険金520万円</span>を提示した。<br>　労働能力喪失率が後遺障害に伴う損害に影響を与えるので、この点につき、保険会社と粘り強く交渉し、<span class="bg-line">保険金850万円</span>の支払いを勝ち取った。</p>
+				<p>　依頼者が自転車で道路を横断中、トラックに側面から衝突され、<span class="bg-line">右足関節の機能障害他の後遺障害</span>が残り、<span class="bg-line">併合11級と認定</span>された。保険会社は未払分として
+					<span class="bg-line">保険金520万円</span>を提示した。<br>　後遺障害が将来得る収入に影響を与えるので、この点につき、保険会社と粘り強く交渉し、<span class="bg-line">保険金850万円</span>の支払いを勝ち取った。</p>
+			</div>
+			<div class="cases">
+				<h4>後遺障害の事故</span>
+				</h4>
+				<div class="img-wrapper"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/traffic-acc-cases_03.png'; ?>"
+					    alt="330万円の増額"></div>
+				<p>　依頼者は、バイクで交差点を直進中、右折中の自動車と衝突し、<span class="bg-line">左腕神経麻痺・機能障害と右手首骨折・機能障害等の重症</span>を負った。<span class="bg-line">後遺障害等級5級</span>が認められたので、これにしたがって、当事務所で最大限の損害額を見積もり、請求した。<br>　他方、過失割合も争いになっていたが、依頼者が認めざるを得ない最小限の割合にとどめ、和解した。治療費等で既に支払われている額を差し引いて、依頼者は約4,700万円を得ることができた。</p>
 			</div>
 			<div class="cases">
 				<h4>死亡事故</h4>
 				<div class="img-wrapper"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/traffic-acc-cases_01.png'; ?>"
 					    alt="330万円の増額"></div>
-				<p>　被害者（80代半ば。無職）は、信号のない交差点にて横断歩道を歩行中に、左方より乗用車にはねられた。<span class="bg-line">当方過失ゼロ。</span>頭部打撲裂創、外傷性くも膜下出血。約5ヶ月入院後、死亡。
+				<p>　被害者（80代半ば。無職）は、信号のない交差点にて横断歩道を歩行中に、左方より自動車にはねられた。<span class="bg-line">当方過失ゼロ。</span>頭部打撲裂創、外傷性くも膜下出血により約5ヶ月入院後、死亡。
 					<span class="bg-line">当事務所において裁判基準に基づいて計算</span>したところ、<span class="bg-line">保険会社提示額より1,000万円強の増額</span>となり、保険会社もこれを認めた。</p>
-			</div>
-			<div class="cases">
-				<h4>バイク対自動車</span>
-				</h4>
-				<div class="img-wrapper"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/traffic-acc-cases_03.png'; ?>"
-					    alt="330万円の増額"></div>
-				<p>　依頼者は、バイクで交差点を直進中、右折中の自動車と衝突し、<span class="bg-line">左腕神経麻痺・機能障害と右手首骨折・機能障害等の重症</span>を負った。<span class="bg-line">後遺障害等級5級</span>が認められたので、これにしたがって、当事務所で最大限の損害額を見積もり、請求した。<br>　他方、過失割合も争いになっていたが、依頼者が認めざるを得ない最小限の割合にとどめ、和解した。
-					<span class="bg-line">最終的な和解額は、約6,400万円。</span>このうち、治療費等で既に支払われている額を差し引いて、依頼者は約4,700万円を得ることができた。（※左胸のほか、手首にも後遺障害あり）</p>
 			</div>
 			<p class="text-center">その他、交通事故関係の解決事例は<a href="<?php echo DTDSH_HOME_URL; ?>cases-category/traffic-acc" target="_blank" title="解決事例　カテゴリー：交通事故"
 				    class="link-external">解決事例　カテゴリー：交通事故</a>に多数掲載しております。</p>
@@ -99,8 +98,8 @@
 <section class="jiko-cta_wrap">
 	<div class="row align-middle">
 		<div class="column small-12 medium-9 large-10">
-			<h2 class="jiko-cta_title">交通事故に遭われたら<br><strong class="color-red font-2x">お早め</strong>に弁護士に相談しましょう。</h2>
-			<p class="jiko-cta_desc">症状が固定されていない早期からご相談いただくと、弁護士が代わりに交渉などを行うことで治療に専念できるメリットがあります。<br>さらに、適切な後遺障害を認定してもらえるよう通院についてのアドバイス、必要な場合は医師宛の書面を作成したり、直接医師と面談し、診断書の内容についての理解を求めることができます。</p>
+			<h2 class="jiko-cta_title">交通事故に遭われたら<br><strong class="color-red font-2x">お早め</strong>に弁護士へご相談ください。</h2>
+			<p class="jiko-cta_desc">事故後早期の段階からご相談・ご依頼いただくと、弁護士が代わりに交渉などを行うことで治療に専念できるメリットがあります。<br>さらに、適切な後遺障害等級を認定してもらえるよう通院についてのアドバイス、必要な場合は医師宛の書面を作成したり、直接医師と面談し、後遺障害診断書の内容についての理解を求めたりすることができます。</p>
 		</div>
 		<div class="column small-12 medium-3 large-2 text-center"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/lp/jiko/soudan-free.png'; ?>"
 			    alt="交通事故に遭われたらお早めに弁護士へ無料の相談しましょう。"></div>
@@ -117,38 +116,36 @@
 			<h3 class="jiko-section_title -sub">交通事故に強い山下江法律事務所のサポート</h3>
 			<div class="jiko-support_wrap bg-grafpaper">
 				<div class="jiko-support">
-					<h4 class="jiko-support_title">初期の費用という負担をなくしました</h4>
+					<h4 class="jiko-support_title">初期の費用負担をなくしました</h4>
 					<p class="jiko-support_desc">弁護士に頼むと多額の費用が必要なのでは？というご心配を解消すべく、相談料無料・着手金無料で交通事故問題の解決を依頼できます。</p>
 				</div>
 				<div class="jiko-support">
 					<h4 class="jiko-support_title">通院についてのアドバイス</h4>
-					<p class="jiko-support_desc">ケガなどで通院が必要な方には、適切な後遺障害が認定されるようアドバイスします。</p>
+					<p class="jiko-support_desc">ケガなどで通院が必要な方には、後遺障害が残りそうな場合には適切な後遺障害等級が認定されるようアドバイスします。</p>
 				</div>
 				<div class="jiko-support">
 					<h4 class="jiko-support_title">保険会社との交渉</h4>
-					<p class="jiko-support_desc">弁護士が適切な賠償金（慰謝料・給与損害・遺失利益など）によるきちんとした補償を受けられるよう、保険会社との交渉をすべて行いますので、弁護士に任せて治療に専念できます。</p>
+					<p class="jiko-support_desc">弁護士が適切な賠償金（慰謝料・休業8損害・逸失利益など）を受けられるよう、保険会社との交渉をすべて行いますので、弁護士に任せて治療に専念できます。</p>
+				</div>
+				<div class="jiko-support">
+					<h4 class="jiko-support_title">症状固定の判断を見極めます</h4>
+					<p class="jiko-support_desc">治療を続けても大幅な改善が見込めなくなった段階のことを「症状固定」といいます。<br>症状固定とされると治療が打ち切られるなど、損害賠償の額に大きく影響するため、症状固定日をいつにするかは非常に大きな意味を持ちます。<br>適切な時期を症状固定日とするよう弁護士が交渉します。</p>
 				</div>
 				<div class="jiko-support">
 					<h4 class="jiko-support_title">後遺障害認定のサポート</h4>
-					<p class="jiko-support_desc">適切な等級で後遺障害を認定してもらうためにはどのような書類を用意すべきか、適切な記述があるか。これらを判断するのには一定以上の知識・経験が必要です。<br><span class="bg-line">準備が不十分であったために等級を獲得できなかったといった事態を避けるため</span>、等級認定手続を経る前に、弁護士に一度相談されることをお勧めいたします。</p>
+					<p class="jiko-support_desc">適切な後遺障害等級を認定してもらうためには、一定水準以上の知識・経験が必要です。<br><span class="bg-line">準備が不十分であったために後遺障害等級を獲得できないといった事態を避けるため</span>、等級認定手続を経る前に、弁護士に一度相談されることをお勧めいたします。</p>
 				</div>
 				<div class="jiko-support">
 					<h4 class="jiko-support_title">必要に応じて医師と直接面談</h4>
-					<p class="jiko-support_desc">医師宛の書面を作成したり、必要に応じて直接医師と面談したりします。<br>このようにして、適切な等級での後遺障害を認定されるようサポートします。</p>
+					<p class="jiko-support_desc">医師宛の書面を作成したり、必要に応じて医師と直接面談したりします。<br>適切な後遺障害等級を認定されるようサポートします。</p>
 				</div>
 				<div class="jiko-support">
-					<h4 class="jiko-support_title">症状固定という重要な判断を見極めます</h4>
-					<p class="jiko-support_desc">治療を続けても大幅な改善が見込めなくなった段階のことを「症状固定」といいます。<br>この症状固定は、事故による損害賠償の額に大きく影響するため、非常に大きな意味を持ちます。<br>最悪の場合、治療継続が必要なのに、治療費を打ち切られて、訴訟になっても裁判所が認めてくれないこともあります。<br>とても重要な判断ですから、
-						<span class="bg-line">専門家である弁護士が判断を後押し</span>をします。</p>
+					<h4 class="jiko-support_title">すでに後遺障害認定結果が出てしまった方でも異議申立可能</h4>
+					<p class="jiko-support_desc">後遺障害認定結果が出てしまった方でも、それが適切ではない場合、認定結果の異議申立を求めることができます。<br>相応の準備が必要ですので、新たな後遺障害診断書を用意するなど万全の体制で臨みます。</p>
 				</div>
 				<div class="jiko-support">
-					<h4 class="jiko-support_title">すでに後遺障害認定結果が出てしまった方でも見直し可能！</h4>
-					<p class="jiko-support_desc">後遺障害認定結果が出てしまった方でも、それが適切ではない場合、認定結果の見直しを求められます。<br>実際に、弁護士介入後、病院（医師）のもとへ同行したことで、<span class="bg-line">後遺障害が認められなかったケースでも</span>、後遺障害の認定を受けました。</p>
-				</div>
-				<div class="jiko-support">
-					<h4 class="jiko-support_title">適切な賠償金（慰謝料・給与損害・遺失利益など）の増額には「交渉」が不可欠</h4>
-					<p class="jiko-support_desc">増額するためには、保険会社との粘り強い交渉が不可欠です。依頼者に代わってその「交渉」をできるのは、弁護士のみです。<br><a href="#section-1">慰謝料などの増額</a>にあるように、弁護士は一般的に高額である③裁判基準をもとに適切な賠償金まで増額して示談交渉します。<br>弁護士が介入することにより、多くの場合は賠償金が増額しますので、
-						<u>弁護士報酬を考慮しても、弁護士不介入の場合に比べて、多くの賠償金を得ることができます</u>。</p>
+					<h4 class="jiko-support_title">適切な賠償金（慰謝料・休業損害・逸失利益など）の増額には「交渉」が不可欠</h4>
+					<p class="jiko-support_desc">増額するためには、保険会社との粘り強い交渉が不可欠です。依頼者に代わってその「交渉」をできるのは、弁護士のみです。<br>弁護士は裁判基準をもとに適切な賠償金が得られるよう示談交渉します。<br>弁護士が介入することにより、多くの場合は賠償金が増額しますので、弁護士報酬を考慮しても、弁護士不介入の場合に比べて、多くの賠償金を得ることができます。</p>
 				</div>
 			</div>
 		</div>
@@ -159,20 +156,19 @@
 		<h2 class="jiko-section_title column small-12">山下江法律事務所の特長</h2>
 		<div class="column small-12 article-body">
 			<h3>1. 広島で多数の解決実績があります</h3>
-			<p>平成23年以降の交通事故案件での実績は、<strong class="bg-line">相談数1,948件、受任件数817件</strong>（平成28年度末現在）です。地元広島における法律事務所で、これだけ多数の相談件数、受任件数の実績があります。</p>
-			<h3>2. 後遺障害など医学的知識があります</h3>
+			<p>平成23年以降の交通事故案件での実績は、<strong class="bg-line">相談数1,948件、受任件数817件</strong>（平成28年度末現在）です。広島における法律事務所で、これだけ多数の相談件数、受任件数の実績があります。</p>
+			<h3>2. 後遺障害などに関する医学的知識があります</h3>
 			<p class="jiko-images"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/jiko-learning.jpg'; ?>" alt="被害者に最も有利な解決を実現するため、医師による医学的知識の習得・研修をおこなっております。"></p>
 			<p>交通事故は多くの場合、身体的傷害を伴うものです。その傷害の回復（治療）をどのようにするのか、その傷害がもたらす後遺障害はどうなるのかは、被害者にとって一番の問題です。被害者のそうした<span class="bg-line">悩みに答え、最高の解決を得られる</span>ようにするため、私たちは医師による医学的知識の習得・研修も行っております。</p>
 			<h3>3. 交通事故チームという専門チームがあります</h3>
 			<p class="jiko-images"><img src="//hiroshima-jiko.com/img/JTkaigi.jpg" alt="交通事故チームによって集積・分析された多数の解決データを共有化し、高い専門性を事務所全体で活用しています。"></p>
-			<p>後遺障害の認定や事故の相手方保険会社との交渉には、高い専門性と経験が求められます。当事務所における多数の解決事例データを集積・分析し、事務所全体で共有化することで被害者に最も有利な解決を実現するため、交通事故チームを結成しています。</p>
-			<h3>4. 事故直後から治療・解決までご相談ください</h3>
-			<p>交通事故に遭われた場合、早い段階で弁護士にご相談いただければ、通院についてのアドバイスや適切な等級の後遺障害認定にあたってのアドバイスを行い、必要に応じて病院（医師）まで同行します。すでに認定されてしまった後遺障害を覆すのは難しい場合があるので、
-				<span class="bg-line">なるべく早い段階でご相談</span>ください。<br>あなたは一人ではありません。私たち弁護士が治療から解決まで寄り添います。</p>
+			<p>後遺障害等級の認定や事故の相手方保険会社との交渉には、高い専門性と経験が求められます。被害者に最も有利な解決を実現するため、交通事故チームを結成し、当事務所における多数の解決事例のデータを集積・分析し、事務所全体で共有しています。</p>
+			<h3>4. 事故直後からのご依頼に対応しています</h3>
+			<p>交通事故に遭われた場合、早い段階で弁護士にご相談・ご依頼いただければ、通院や適切な後遺障害等級認定にあたってのアドバイスを行い、必要に応じて医師と面談します。治療が不適切である場合、適切な後遺障害等級を得ることが難しくなるので、治療開始から間もない<span class="bg-line">なるべく早い段階でご相談</span>ください。<br>あなたは一人ではありません。私たち弁護士が治療から解決まで寄り添います。</p>
 			<h3>5. 相談料や着手金など初期費用面で負担がありません</h3>
 			<p class="jiko-images"><img src="//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/rates/jiko-sodan.png" alt="被害者救済のため、被害者の相談料は無料です。">
 				<img src="//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/rates/jiko-retainer.png" alt="被害者救済のため、被害者の相談料は無料です。"></p>
-			<p>被害者救済のため、交通事故に遭われた方の<strong class="bg-line">相談料・着手金は無料</strong>です。（弁護士費用特約により保険会社から支払われる場合は別）</p>
+			<p>被害者救済のため、交通事故に遭われた方の<strong class="bg-line">相談料・着手金は無料</strong>です。（弁護士費用特約のある方は<a href="#section-6">こちら</a>をご覧ください。）</p>
 		</div>
 	</div>
 </section>
@@ -218,7 +214,7 @@
 <section id="section-6" class="jiko-section" data-magellan-target="section-6">
 	<div class="row">
 		<div class="column small-12">
-			<h2 class="jiko-cta_title text-center">「弁護士費用特約」が付いていれば<br><span class="font-2x color-red">弁護士費用</span>が<span class="font-2x color-red">無料</span>に!</h2>
+			<h2 class="jiko-cta_title text-center">「弁護士費用特約」が付いていれば<br><span class="font-2x color-red">弁護士費用</span>が基本的に<span class="font-2x color-red">無料</span>!</h2>
 			<p class="jiko_cta_desc p2">弁護士に依頼すると、費用が高いイメージから相談しづらいという被害者の方もいらっしゃると思います。しかし、保険の特約に「弁護士費用特約」が付いていれば、その中でまかなわれる場合が多いです。</p>
 			<p class="p1 bg-gainsboro">特殊なケースを除けば保険会社が支払うので、基本的に<span class="bg-line">お客様自身のご負担はありません。</span>実費や出張日当などの費用についても弁護士費用特約から支出されます。<br>多くの保険会社では、弁護士費用の総額が300万円までと上限が定めてありますが、弁護士費用が300万円以上になるようなケースはまれですので、
 				<span class="bg-line">多くの場合は弁護士費用特約でカバーできます。</span><br>※弁護士費用特約は保険会社の約款により細かい条件などがあるケースがございます。まずは無料でご相談ください。</p>
@@ -230,7 +226,7 @@
 	<div class="row align-middle">
 		<div class="column small-12 medium-9 large-10">
 			<h2 class="jiko-cta_title">交通事故に遭ったら、<strong class="color-red font-2x">早い段階</strong>で弁護士に相談しましょう。</h2>
-			<p class="jiko-cta_desc">交通事故の発生後、早い段階でご相談いただければ、交渉などをすべて弁護士が代理するので治療に専念できます。<br>また、適切な後遺障害を認定してもらえるよう通院についてのアドバイスもいたします。</p>
+			<p class="jiko-cta_desc">交通事故の発生後、早い段階でご相談・ご依頼いただければ、交渉などをすべて弁護士が代理するので治療に専念できます。<br>また、適切な後遺障害を認定してもらえるよう通院についてのアドバイスもいたします。</p>
 		</div>
 		<div class="column small-12 small-3 large-2 text-center"><img src="<?php echo '//www.law-yamashita.com/wp-content/themes/law-yamashita/assets/img/lp/jiko/soudan-free.png'; ?>"
 			    alt="広島最大級の山下江法律事務所なら交通事故の相談が無料！"></div>
